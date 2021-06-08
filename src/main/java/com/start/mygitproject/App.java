fc5295f5fe8662b85b013b5b9453548ca89f5ae8
SaurabDey <saurab.jeet@gmail.com>
2021-06-08 08:31:23 +0530

    first commit

---
+++ src/main/java/com/start/mygitproject/App.java
@@ -0,0 +1,13 @@
+package com.start.mygitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
